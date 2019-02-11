387eb669f88f976401056f22625c18fc3b4fb911
Iñaki Quenerapú <quenerapu@gmail.com>
2019-02-11 03:35:41 +0100

    Create markdown-sample.php

---
+++ inc/markdown-sample.php
@@ -0,0 +1,37 @@
+<?php /* md
+[title]:              # "[en]Title in English|[es]Título en español|[gal]Título en galego"
+[datetime]:           # "2018-02-10 20:47:00"
+[description]:        # "[en]Description in English|[es]Descripción en español|[gal]Descripción en galego"
+[keywords]:           # "words, palabras e máis palabras"
+[image_file]:         # "https://mini.trece.io/img/trece.jpg"
+[image_description]:  # "[en]Image description|[es]Descripción de la imagen|[gal]Descripción da imaxe"
+
+
+
+[--en--]
+
+# Hello world!!
+
+This page was written in English using *markdown*.
+
+[--/en--]
+
+
+
+[--es--]
+
+# ¡Hola mundo!
+
+Esta página está escrita en español usando *markdown*.
+
+[--/es--]
+
+
+
+[--gal--]
+
+# Ola mundo!
+
+Esta páxina foi escrita en galego usando *markdown*.
+
+[--/gal--]
