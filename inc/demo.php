c403146c37dd99264d6f83ee289373b251c6f5ce
Iñaki Quenerapú <quenerapu@gmail.com>
2019-02-27 09:58:50 +0100

    Create demo.php

---
+++ inc/demo.php
@@ -0,0 +1,66 @@
+<?php /* md
+[title]:              # "Demo"
+[datetime]:           # "2018-02-27 20:47:00"
+[description]:        # "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+[keywords]:           # "lorem ipsum dolor sit amet consectetur adipiscing elit"
+[image_file]:         # "https://fakeimg.pl/1200x600/00ffff/?text=TRECE_demo"
+[image_description]:  # "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+
+
+
+![ALT test](https://fakeimg.pl/1200x400/00ff00/?text=Huge-image-on-top){.fullrow.top}
+
+# Demo
+
+[[DATE]]
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nisl est, lacinia id egestas quis, sollicitudin sit amet velit. Praesent eget convallis nunc. Maecenas id mattis massa. Curabitur quam lorem, facilisis sit amet dapibus at, laoreet a justo. Pellentesque rhoncus rhoncus odio. Praesent eu rhoncus diam, sed tincidunt ligula. Nulla vel viverra diam, vitae tempus mauris. Aliquam venenatis libero magna, eu semper nisi pretium in.
+
+<div class="map-responsive row" style="padding-bottom:0;">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2954.8323814357586!2d-8.73485208416035!3d42.218026979196786!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd2589e20bfa2c5f%3A0xfd3ee5e9e2a37c25!2sBarber%C3%ADa+tradicional+Savoy!5e0!3m2!1ses!2ses!4v1551097492715" width="600" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+</div>
+
+Maecenas sit amet feugiat arcu. Nam eu euismod sapien, sed egestas sapien. Aenean tincidunt purus eget leo eleifend mollis. Duis quis metus diam. Aenean tincidunt enim purus, at semper libero sodales sodales. Donec commodo magna et diam fringilla dignissim. Vestibulum eget arcu venenatis, elementum odio et, tempor metus. Mauris non mattis nisl. Integer malesuada interdum tellus, at vehicula ex ultrices a. Nulla ut odio et elit rutrum pellentesque at et ante. Mauris vestibulum dapibus dui, eu malesuada turpis scelerisque nec. Fusce rutrum molestie libero at varius.
+
+![ALT test](https://fakeimg.pl/1200x600/00ffff/?text=Big-image){.row}
+
+Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi pellentesque augue at eros maximus, sit amet imperdiet ipsum pharetra. Etiam imperdiet, ipsum nec rutrum luctus, nisl nunc tristique nibh, et condimentum mauris dolor non erat. Aliquam eget odio euismod, euismod dolor at, lacinia urna. In laoreet eu turpis ac vestibulum. In quam metus, ultrices sed velit ac, vulputate rutrum justo. Nam pharetra porta est, sit amet tempus libero sagittis ac. Nulla facilisi. Quisque molestie sapien vitae sem sagittis, sit amet vehicula lorem egestas.
+
+<div class="map-responsive fullrow" style="padding-bottom:0;">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2954.8323814357586!2d-8.73485208416035!3d42.218026979196786!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd2589e20bfa2c5f%3A0xfd3ee5e9e2a37c25!2sBarber%C3%ADa+tradicional+Savoy!5e0!3m2!1ses!2ses!4v1551097492715" width="600" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>
+</div>
+
+Fusce fermentum quis sem at sagittis. Nulla dapibus congue luctus. Vivamus faucibus pharetra ligula. Vestibulum tincidunt eget ex in pharetra. Aenean non luctus eros. Praesent porta turpis eleifend, sollicitudin felis quis, iaculis arcu. Quisque ultrices fringilla tempor. Maecenas at quam in ante tincidunt condimentum vel non magna. Sed felis nisl, dignissim eget finibus et, tristique vel diam. Proin convallis massa in sapien tempus tristique.
+
+<div class="gallery" markdown=1>
+
+![ALT test](https://fakeimg.pl/1200x600/00ffff/?text=Gallery-image-1){.left}
+![ALT test](https://fakeimg.pl/1200x600/00ffff/?text=Gallery-image-2){.right}
+![ALT test](https://fakeimg.pl/1200x600/00ffff/?text=Gallery-image-3){.left}
+![ALT test](https://fakeimg.pl/1200x600/00ffff/?text=Gallery-image-4){.right}
+![ALT test](https://fakeimg.pl/1200x600/00ffff/?text=Gallery-image-5){.left}
+
+</div>
+
+Ut ut augue mattis, lobortis eros a, ultricies ante. Proin ut purus sed nibh vulputate convallis. Curabitur consequat pharetra finibus. Nulla rutrum venenatis eleifend. Morbi dapibus massa in consectetur iaculis. Quisque eget venenatis metus. Maecenas dignissim finibus fermentum. Pellentesque consectetur nibh augue. Morbi scelerisque magna nulla, eu venenatis nibh tristique non.
+
+## Hello world!
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam nisl est, lacinia id egestas quis, sollicitudin sit amet velit. Praesent eget convallis nunc. Maecenas id mattis massa. Curabitur quam lorem, facilisis sit amet dapibus at, laoreet a justo. Pellentesque rhoncus rhoncus odio. Praesent eu rhoncus diam, sed tincidunt ligula. Nulla vel viverra diam, vitae tempus mauris. Aliquam venenatis libero magna, eu semper nisi pretium in.
+
+### Hello world!
+
+Maecenas sit amet feugiat arcu. Nam eu euismod sapien, sed egestas sapien. Aenean tincidunt purus eget leo eleifend mollis. Duis quis metus diam. Aenean tincidunt enim purus, at semper libero sodales sodales. Donec commodo magna et diam fringilla dignissim. Vestibulum eget arcu venenatis, elementum odio et, tempor metus. Mauris non mattis nisl. Integer malesuada interdum tellus, at vehicula ex ultrices a. Nulla ut odio et elit rutrum pellentesque at et ante. Mauris vestibulum dapibus dui, eu malesuada turpis scelerisque nec. Fusce rutrum molestie libero at varius.
+
+#### Hello world!
+
+Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi pellentesque augue at eros maximus, sit amet imperdiet ipsum pharetra. Etiam imperdiet, ipsum nec rutrum luctus, nisl nunc tristique nibh, et condimentum mauris dolor non erat. Aliquam eget odio euismod, euismod dolor at, lacinia urna. In laoreet eu turpis ac vestibulum. In quam metus, ultrices sed velit ac, vulputate rutrum justo. Nam pharetra porta est, sit amet tempus libero sagittis ac. Nulla facilisi. Quisque molestie sapien vitae sem sagittis, sit amet vehicula lorem egestas.
+
+##### Hello world!
+
+Fusce fermentum quis sem at sagittis. Nulla dapibus congue luctus. Vivamus faucibus pharetra ligula. Vestibulum tincidunt eget ex in pharetra. Aenean non luctus eros. Praesent porta turpis eleifend, sollicitudin felis quis, iaculis arcu. Quisque ultrices fringilla tempor. Maecenas at quam in ante tincidunt condimentum vel non magna. Sed felis nisl, dignissim eget finibus et, tristique vel diam. Proin convallis massa in sapien tempus tristique.
+
+###### Hello world!
+
+Ut ut augue mattis, lobortis eros a, ultricies ante. Proin ut purus sed nibh vulputate convallis. Curabitur consequat pharetra finibus. Nulla rutrum venenatis eleifend. Morbi dapibus massa in consectetur iaculis. Quisque eget venenatis metus. Maecenas dignissim finibus fermentum. Pellentesque consectetur nibh augue. Morbi scelerisque magna nulla, eu venenatis nibh tristique non.
+
