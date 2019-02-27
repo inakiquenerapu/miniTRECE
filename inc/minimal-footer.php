f504cc26075960ac2e461fcfd61f22545adce412
Iñaki Quenerapú <quenerapu@gmail.com>
2019-02-27 09:58:00 +0100

    Create minimal-footer.php

---
+++ inc/minimal-footer.php
@@ -0,0 +1,15 @@
+  </article>
+
+  <footer>
+
+    <div id="footer">
+
+      <p>Minimal theme for <a href="https://mini.trece.io">miniTRECE</a> by <a href="https://quenerapu.com">@Quenerapu</a></p>
+
+    </div>
+
+  </footer>
+
+<?=BEGRATEFUL;?>
+</body>
+</html>
