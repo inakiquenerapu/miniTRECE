ed6aeb0708e066e58fa6ff3c0cd9d5181a4d8119
Rodrigo <dev@brunneis.com>
2019-05-05 20:44:27 +0200

    Persistent storage for Docker container

--- docker/build.sh
+++ docker/build.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-tar --dereference -cf minitrece.tar \
-    ../trece \
-    ../css \
-    ../img \
-    .htaccess \
-    ../LICENSE \
-    ../README.md \
-    ../index.php
-docker build -t minitrece -f Dockerfile ..
-rm minitrece.tar
+docker build -t minitrece .
--- docker/launch.sh
+++ docker/launch.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+DOCKER_IMAGE=minitrece
 docker rm -f minitrece 2>/dev/null
-docker run -tid -p 80:80 --name minitrece minitrece
+if [ ! -d "htdocs" ]; then
+    mkdir htdocs
+    cp .htaccess htdocs
+    cp ../LICENSE htdocs
+    cp ../README.md htdocs
+    cp ../index.php htdocs
+    cp -r ../trece htdocs
+    cp -r ../css htdocs
+    cp -r ../img htdocs
+fi
+docker run -d \
+-p 80:80 \
+-v "$(pwd)"/htdocs:/var/www/html:Z \
+--name $DOCKER_IMAGE minitrece
