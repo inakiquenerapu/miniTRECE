01712954fa22d0446cd3bac17bfc34571f1340b0
Rodrigo <dev@brunneis.com>
2019-05-05 22:22:16 +0200

    Improved container launcher

--- docker/launch.sh
+++ docker/launch.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-DOCKER_IMAGE=minitrece
+source env.sh
 docker rm -f minitrece 2>/dev/null
+run_container () {
+    docker run -d \
+    -e SITE_SCHEME=$SITE_SCHEME \
+    -p $PORT:80 \
+    -v "$(pwd)"/htdocs:/var/www/html:z \
+    --name $CONTAINER_NAME $DOCKER_IMAGE
+}
 if [ ! -d "htdocs" ]; then
     mkdir htdocs
     cp .htaccess htdocs
@@ -11,8 +18,4 @@ if [ ! -d "htdocs" ]; then
     cp -r ../css htdocs
     cp -r ../img htdocs
 fi
-docker run -d \
--e SITE_SCHEME=https \
--p 80:80 \
--v "$(pwd)"/htdocs:/var/www/html:Z \
---name $DOCKER_IMAGE minitrece
+run_container
