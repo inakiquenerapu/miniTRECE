30b377776c6f4a1c7c96387382c9340417e89317
Iñaki Quenerapú <quenerapu@gmail.com>
2019-05-06 06:10:06 +0200

    Update minimal_footer.php

--- trece/inc/minimal_footer.php
+++ trece/inc/minimal_footer.php
@@ -4,7 +4,7 @@
 
     <div id="footer">
 
-      <p>Minimal theme for <a href="https://mini.trece.io">miniTRECE</a> by <a href="https://quenerapu.com">@Quenerapu</a></p>
+      <p>Minimal theme for <a href="https://mini.trece.io">miniTRECE</a> by <a href="https://github.com/quenerapu">@Quenerapu</a></p>
 
     </div>
 
