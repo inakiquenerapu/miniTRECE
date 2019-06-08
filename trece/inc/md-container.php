95a2fbcd3fe3eab4d99219512a4e829274c2e3ef
Iñaki Quenerapú <quenerapu@gmail.com>
2019-06-08 11:13:01 +0200

    Create md-container.php

---
+++ trece/inc/md-container.php
@@ -0,0 +1,9 @@
+  <div class="container main-container">
+
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2">
+        <?=$markdownStuff;?>
+      </div>
+    </div>
+
+  </div>
